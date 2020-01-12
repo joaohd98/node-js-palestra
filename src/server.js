54210d0e7b0980e23f12b7a8b3c97dc185394403
João Damazio <joaohd98@gmail.com>
2020-01-11 22:29:54 -0300

    Remove unnecessary comments

--- src/server.js
+++ src/server.js
@@ -8,30 +8,3 @@ app.use(require("./middleware/access-control"));
 app.use(require("./api/routes/router").router);
 
 app.listen(process.env.PORT || 3030);
-
-
-/*
-//GET API
-app.get("/api/user", function(req , res){
-    var query = "select * from [user]";
-    executeQuery (res, query);
-});
-
-//POST API
-app.post("/api/user", function(req , res){
-    var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password”);
-    executeQuery (res, query);
-});
-
-//PUT API
-app.put("/api/user/:id", function(req , res){
-    var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
-    executeQuery (res, query);
-});
-
-// DELETE API
-app.delete("/api/user /:id", function(req , res){
-    var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
-    executeQuery (res, query);
-});
-*/
